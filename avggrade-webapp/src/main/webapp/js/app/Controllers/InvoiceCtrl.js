27e866b262b0b57784b1a06244ce27ad515a758e
Kristjan Veskimae <kristjan.veskimae@gmail.com>
2015-12-23 18:52:18 +0200

    Present calculated average grades to user

--- avggrade-webapp/src/main/webapp/js/app/Controllers/InvoiceCtrl.js
+++ avggrade-webapp/src/main/webapp/js/app/Controllers/InvoiceCtrl.js
@@ -16,22 +16,6 @@
             $scope.appData.extractionInProgress = true;
             $rootScope.$broadcast(events.message._SEND_TO_PROCESS_, [files]);
         };
-        $scope.presentPaymentLink = function () {
-        	var accountNr = window.encodeURIComponent($scope.selectedBank.selectedValue.accountNr);
-        	var total = window.encodeURIComponent($scope.appData.result.total);
-        	var name = window.encodeURIComponent($scope.appData.result.name);
-        	var referenceNr = window.encodeURIComponent($scope.appData.result.referenceNr);
-        	var description = window.encodeURIComponent($scope.appData.result.description);
-        	var link = 
-        		"http://www.mybank.ee/banklink?"+
-        			"VK_ACC="+accountNr+
-        			"&VK_AMOUNT=" + total +
-        			"&VK_NAME=" +  name +
-        			"&VK_REF=" + referenceNr +
-        			"&VK_MSG=" + description;
-            $scope.reset();
-            $scope.appData.link = link;
-        };
         $scope.selectedBank = {selectedValue: null};
         $scope.updateAccountOrdering = function() {
         	var orderedBanks = $cookieStore.get('orderedBanks');
@@ -48,40 +32,7 @@
         	$cookieStore.put('orderedBanks', orderedBanks);
         };
         $rootScope.$on(events.message._SEND_TO_PROCESS_COMPLETE_, function (event, data) {
-        	if ($scope.appData.result.accountNr.length > 0) {
-            	var orderedBanks = $cookieStore.get('orderedBanks');
-                if (!orderedBanks) {
-                	orderedBanks = [];
-                	angular.forEach($scope.appData.result.accountNr, function(i) {
-                	  this.push(i.bankCorpororation);
-                	}, orderedBanks);
-                } else {
-                	angular.forEach($scope.appData.result.accountNr, function(i) {
-                		if (this.indexOf(i.bankCorpororation) == -1) {
-                      	  this.push(i.bankCorpororation);	
-                		}
-                	}, orderedBanks);
-                }
-            	$cookieStore.put('orderedBanks', orderedBanks);
-            	var lastIdx = -1;
-            	angular.forEach(orderedBanks, function(val, idx) {
-            		lastIdx = idx + 1;
-                	angular.forEach($scope.appData.result.accountNr, function(i) {
-                		if (i.bankCorpororation == val) {
-                      	  i.sortIdx = idx;	
-                		}
-                	});
-            	});
-            	angular.forEach($scope.appData.result.accountNr, function(i) {
-            		if (i.sortIdx === null) {
-            			i.sortIdx = lastIdx;	
-            		}
-            	});
-            	$scope.appData.result.accountNr.sort(function(a, b){return a.sortIdx - b.sortIdx});
-            	$scope.selectedBank.selectedValue = $scope.appData.result.accountNr[0];
-        	} else {
-        		$scope.selectedBank.selectedValue = null;
-        	}
+
         });
     });
 })();
